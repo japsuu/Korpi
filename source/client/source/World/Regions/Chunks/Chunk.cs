fef40cc09c638a8d91c9098e702ab8b29c13f6d2
Jasper <japsu.honkasalo@gmail.com>
2024-01-11 08:33:14 +0200

    Move ChunkState to own class

--- source/client/source/World/Regions/Chunks/Chunk.cs
+++ source/client/source/World/Regions/Chunks/Chunk.cs
@@ -12,47 +12,6 @@ using OpenTK.Mathematics;
 
 namespace Korpi.Client.World.Regions.Chunks;
 
-/// <summary>
-/// Represents the state of the chunk.
-/// </summary>
-public enum ChunkState
-{
-    /// <summary>
-    /// The chunk is uninitialized.
-    /// </summary>
-    UNINITIALIZED,
-    
-    /// <summary>
-    /// The chunk is queued for terrain generation.
-    /// </summary>
-    GENERATING_TERRAIN,
-    
-    /// <summary>
-    /// The chunk is queued for decoration generation.
-    /// </summary>
-    GENERATING_DECORATION,
-        
-    /// <summary>
-    /// The chunk is queued for lighting generation.
-    /// </summary>
-    GENERATING_LIGHTING,
-
-    /// <summary>
-    /// The chunk is waiting for neighbouring chunks to be generated.
-    /// </summary>
-    WAITING_FOR_MESHING,
-
-    /// <summary>
-    /// The chunk is queued for meshing.
-    /// </summary>
-    MESHING,
-
-    /// <summary>
-    /// The chunk is ready.
-    /// </summary>
-    READY
-}
-
 public class Chunk
 {
     private readonly IBlockStorage _blockStorage = new BlockPalette();
